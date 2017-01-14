4ce5793327a78d937bd72baefca2b8144f8aeb5d
designergleb <g_leksikov@wargaming.net>
2017-01-14 18:21:36 +0200

    JS Modul 1-2 in progress

---
+++ tech/JS-Modul-1-2/script.js
@@ -0,0 +1,17 @@
+var a = prompt('Введите число которое вы хотите возвести в степень', 1);
+var b = prompt('Введите степень, на которую вы хотите возвести число ' + a, 1);
+var arr = new Array;
+var result = a;
+
+function count() {
+  for (var i = 0; i < b; i++) {
+    arr[i] = a;
+  } 
+  for (var i = 0; i < b - 1; i++) {
+  	var num = arr[i].toString();
+  	result = result * Number(num);
+  }
+  alert("Результать возведения " + a + ' в/во ' + b + ' степень равен ' + result);
+}
+
+count();
