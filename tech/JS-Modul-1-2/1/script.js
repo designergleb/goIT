6404ba3941f08e6f5e91ac0d9fa1a39472b9c03f
designergleb <g_leksikov@wargaming.net>
2017-01-18 21:13:16 +0200

    JS-1 calc improvement

--- tech/JS-Modul-1-2/1/script.js
+++ tech/JS-Modul-1-2/1/script.js
@@ -1,17 +1,33 @@
-var a = prompt('Введите число которое вы хотите возвести в степень', 1);
-var b = prompt('Введите степень, на которую вы хотите возвести число ' + a, 1);
-var arr = new Array;
-var result = a;
+var a = prompt('Введите число которое вы хотите возвести в степень', 0);
+var b = prompt('Введите степень, на которую вы хотите возвести число ' + a, 0);
+var n = a; //Переменная-контейнер для циклического перемножения
+
+function result() {
+	console.log('Результат возведения ' + a + ' в степень ' + b + ' равен ' + n);
+}
 
 function count() {
-  for (var i = 0; i < b; i++) {
-    arr[i] = a;
-  } 
-  for (var i = 0; i < b - 1; i++) {
-  	var num = arr[i].toString();
-  	result = result * Number(num);
-  }
-  console.log('Результат возведения ' + a + ' в степень ' + b + ' равен ' + result);
+	b = Number(b);
+
+	if (isNaN(b)) {
+		console.log('Неверное введены значения. Пожалуйста, введите цифры');
+	} else if (b < 0) {
+		for (var i = b; i < 0 - 1; i++) {
+			n = (n * Number(a));
+			console.log(a);
+		}
+		n = 1 / n;
+		result();
+	} else if (b > 0) {
+		for (var i = 0; i < b - 1; i++) {
+			n = n * Number(a);	
+			console.log(a);
+		} 
+		result();
+	} else if (b == 0) {
+		n = 1;
+		result();
+	}
 }
 
 count();
