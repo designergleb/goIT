4ce8d4dca4bfcc96074f5bb508a7767b25e0857c
designergleb <g_leksikov@wargaming.net>
2017-02-15 18:05:17 +0200

    JS Modul 3-4 / bugfix

--- tech/JS-Modul-3-4/script/script.js
+++ tech/JS-Modul-3-4/script/script.js
@@ -1,9 +1,14 @@
+function _create(name) {
+			return document.createElement(name);
+		}
+
 var quiz = {
 	structure: function() {
-		var div = document.createElement('div');
-		var form = document.createElement('form');
-		var h1 = document.createElement('h1');
-		var button = document.createElement('button');
+		
+		var div = _create('div');
+		var form = _create('form');
+		var h1 = _create('h1');
+		var button = _create('button');
 
 		div.className = "wrapper";
 		document.body.appendChild(div);
@@ -13,9 +18,9 @@ var quiz = {
 
 	    for (var i = 1; i < 4; i++) {
 
-			var fieldset = document.createElement('fieldset');
-			var legend = document.createElement('legend');
-			var ul = document.createElement('ul');
+			var fieldset = _create('fieldset');
+			var legend = _create('legend');
+			var ul = _create('ul');
 
 			form.appendChild(fieldset);
 			fieldset.appendChild(legend);
@@ -25,9 +30,9 @@ var quiz = {
 			
 			for (var j = 1; j < 4; j++) {
 
-				var li = document.createElement('li');
-				var input = document.createElement('input');
-				var label = document.createElement('label');
+				var li = _create('li');
+				var input = _create('input');
+				var label = _create('label');
 
 				ul.appendChild(li);
 				li.appendChild(input);
